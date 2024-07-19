78e435f8387b4af547a1d071b197aba614b6bb12
DineshPawar07 <dineshpawarr07@gmail.com>
2024-07-19 16:39:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Optional JavaScript for additional interactive features
+document.addEventListener("DOMContentLoaded", function() {
+    // Any additional JavaScript functionalities can be added here
+  });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
